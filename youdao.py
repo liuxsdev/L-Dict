50cfb3ec922a9c85fe05493feab5a4e235db4817
smilevent <1028330144@qq.com>
2017-08-20 11:58:40 +0800

    add

--- youdao.py
+++ youdao.py
@@ -4,4 +4,12 @@ import requests
 
 
 def getYoudao(kw):
-    pass
+    url='http://fanyi.youdao.com/openapi.do?keyfrom=dictquery&key=947851566&type=data&doctype=json&version=1.1&q='+kw
+    r=requests.get(url)
+    res=r.json()
+    return res
+
+
+if __name__ == '__main__':
+    a=getYoudao('good')
+    print(a,type(a))
