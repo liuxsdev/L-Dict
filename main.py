cd5f7380c94749fa9aa080c4cf61f8dac0485b44
smilevent <1028330144@qq.com>
2017-08-22 16:19:14 +0800

    继续完善窗口

--- main.py
+++ main.py
@@ -2,27 +2,20 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget,QMainWindow
 from PyQt5.QtGui import QClipboard
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QUrl
+from PyQt5.QtWebKitWidgets import QWebView
 from youdao import getYoudao,printYoudao
-from PyQt5.QtWebKitWidgets import *
-from PyQt5.QtCore import QUrl
 
-
-class MianWindow(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        #self.initUI()
+        self.initUI()
         self.addClipbordListener()
 
-    def initUI(self,html):   
+    def initUI(self):   
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('Tooltips')   
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.browser = QWebView()
-        url = 'file:///H:/workspace/web/app/Dict/index.html'
-        #self.browser.setUrl(QUrl(url))
-        self.browser.setHtml(html)
-        self.setCentralWidget(self.browser)
+        # self.setWindowFlags(Qt.FramelessWindowHint)
         self.show()
 
 
@@ -32,22 +25,29 @@ class MianWindow(QMainWindow):
 
     def onClipboradChanged(self):
         clipboard = QApplication.clipboard()
-        printYoudao(getYoudao(clipboard.text()))
-        self.initUI(str(getYoudao(clipboard.text())))
-        # self.browser = QWebView()
-        # url = 'file:///H:/workspace/web/app/Dict/index.html'
-        # # 指定打开界面的 URL
-        # self.browser.setUrl(QUrl(url))
-        # self.browser.setHtml('ds')
-        # self.show()
+        a=getYoudao(clipboard.text())
+        printYoudao(a)
+        self.show = showWin(str(a))
+        self.show.show()
 
 
 
-
+class showWin(QMainWindow):
+    def __init__(self,n):
+        self.n=n
+        super(showWin, self).__init__()
+        self.initUI()
+    def initUI(self):
+        self.resize(280,600)
+        self.browser = QWebView()
+        #url = 'file:///H:/workspace/web/app/Dict/index.html'
+        #self.browser.setUrl(QUrl(url))
+        self.browser.setHtml(self.n)
+        self.setCentralWidget(self.browser)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MianWindow()
+    ex = MainWindow()
     sys.exit(app.exec_())
 
 
