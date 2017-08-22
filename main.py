61e2a6e5d48763374304685732a7a90e7f16b54f
smilevent <1028330144@qq.com>
2017-08-22 13:11:24 +0800

    尝试使用窗口

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget,QMainWindow
 from PyQt5.QtGui import QClipboard
+from PyQt5.QtCore import Qt
 from youdao import getYoudao,printYoudao
+from PyQt5.QtWebKitWidgets import *
+from PyQt5.QtCore import QUrl
 
-class MianWindow(QWidget):
+
+class MianWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         #self.initUI()
         self.addClipbordListener()
 
-    def initUI(self):   
+    def initUI(self,html):   
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('Tooltips')   
-        # self.show()
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.browser = QWebView()
+        url = 'file:///H:/workspace/web/app/Dict/index.html'
+        #self.browser.setUrl(QUrl(url))
+        self.browser.setHtml(html)
+        self.setCentralWidget(self.browser)
+        self.show()
+
 
     def addClipbordListener(self):
         clipboard = QApplication.clipboard()
@@ -22,7 +33,17 @@ class MianWindow(QWidget):
     def onClipboradChanged(self):
         clipboard = QApplication.clipboard()
         printYoudao(getYoudao(clipboard.text()))
-        
+        self.initUI(str(getYoudao(clipboard.text())))
+        # self.browser = QWebView()
+        # url = 'file:///H:/workspace/web/app/Dict/index.html'
+        # # 指定打开界面的 URL
+        # self.browser.setUrl(QUrl(url))
+        # self.browser.setHtml('ds')
+        # self.show()
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
