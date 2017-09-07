9ddc9072eee9fd84e99e29366b0010367a5374ea
smilevent <1028330144qq.com>
2017-09-07 23:09:50 +0800

    保持到SQLite

--- save.py
+++ save.py
@@ -1,50 +1,26 @@
-# 导入:
-from sqlalchemy import *
-from sqlalchemy.orm import *
-from sqlalchemy.ext.declarative import declarative_base
+# -*-coding:utf-8 -*-
+from youdao import getYoudao
+import sqlite3
+import time
 
-# 创建对象的基类:
-Base = declarative_base()
+conn=sqlite3.connect('test.db')
+cu=conn.cursor()
 
-# 定义User对象:
-class User(Base):
-    # 表的名字:
-    __tablename__ = 'user'
+sql_createTable='CREATE TABLE IF NOT EXISTS word(id integer PRIMARY KEY AUTOINCREMENT,name text UNIQUE,explains text,initials text,addtime text,searchtimes text,groups text)'
 
-    # 表的结构:
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(20),unique=True)
+cu.execute(sql_createTable)
 
 
 
-
-
-
-# 初始化数据库连接:
-engine = create_engine('sqlite:///./dict/test.db')
-# 创建DBSession类型:
-Base.metadata.create_all(engine)
-DBSession = sessionmaker(bind=engine)
-
-def insert(ob):
-    # 创建session对象:
-    session = DBSession()
-    # 创建新User对象:
-    #new_user = User(name='w4s')
-    #添加到session:
-    session.merge(ob)
-    #session.add(new_user)
-    # 提交即保存到数据库:
+def insert(name=None,explains=None,initials=None,addtime=None,searchtimes=None,groups=None):
+    addtime=str(time.time())
+    initials=name[0].capitalize()
+    sql="INSERT INTO word(name,explains,initials,addtime,searchtimes,groups) VALUES (?,?,?,?,?,?)"
     try:
-        session.commit()
-        print('insert success!')
-    except:
-        print('something error')
-    # 关闭session:
-    session.close()
-    
-new=User(name='w4ss')
-
-insert(new)
-
-
+        cu.execute(sql,(name,explains,initials,addtime,searchtimes,groups))
+    except sqlite3.IntegrityError as e:
+        print(e,'exists!')
+ 
+insert('go',str(getYoudao('go')))
+cu.close()
+conn.commit()
