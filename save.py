b83f359bc53d3fc867f9b0bb8edd23576d3e8598
smilevent <1028330144qq.com>
2017-09-15 23:56:43 +0800

    ->

--- save.py
+++ save.py
@@ -3,7 +3,7 @@ from youdao import getYoudao
 import sqlite3
 import time
 
-conn=sqlite3.connect('test.db')
+conn=sqlite3.connect('./Dict/test.db')
 cu=conn.cursor()
 
 sql_createTable='CREATE TABLE IF NOT EXISTS word(id integer PRIMARY KEY AUTOINCREMENT,name text UNIQUE,explains text,initials text,addtime text,searchtimes text,groups text)'
@@ -12,15 +12,21 @@ cu.execute(sql_createTable)
 
 
 
-def insert(name=None,explains=None,initials=None,addtime=None,searchtimes=None,groups=None):
-    addtime=str(time.time())
-    initials=name[0].capitalize()
+def insert_toDB(name=None,explains=None,initials=None,addtime=None,searchtimes=None,groups=None):
+    addtime=str(time.time())   #添加时间
+    initials=name[0].capitalize()  #首字母
     sql="INSERT INTO word(name,explains,initials,addtime,searchtimes,groups) VALUES (?,?,?,?,?,?)"
     try:
         cu.execute(sql,(name,explains,initials,addtime,searchtimes,groups))
     except sqlite3.IntegrityError as e:
         print(e,'exists!')
- 
-insert('go',str(getYoudao('go')))
+def query_inDB(word):
+    SQL="SELECT name,explains from word WHERE name='%s'" % word
+    cu.execute(SQL)
+    res=cu.fetchall()
+    print(res)
+
+
+query_inDB('go')
 cu.close()
 conn.commit()
